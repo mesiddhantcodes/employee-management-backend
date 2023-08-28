2e52cbea39e13fab9c34e269dd4c68c41b9ab030
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-08-28 18:52:47 +0530

    updated

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/auth');
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', usersRouter);
 module.exports = app;
